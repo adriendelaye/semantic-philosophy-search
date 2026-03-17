59c5906559fe06b5aff563db42e20547a1be51ff
adriendelaye <adrien.v.delaye@gmail.com>
2026-03-17 03:02:34 +0100

    Create build-pipeline.py

---
+++ build-pipeline.py
@@ -0,0 +1,13 @@
+from src.scraper import scrape
+from src.preprocess import preprocess_folder
+from src.embedder import build_embeddings
+from src.indexer import build_index
+
+topics = ["meaning", "reference", "semantics", "speech-acts"]
+
+scrape(topics)
+preprocess_folder()
+build_embeddings()
+build_index()
+
+print("Pipeline terminé")
